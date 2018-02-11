58d604c1e187ed5c30c7a1f836c2daedecc8c4b5
kajetans <66szukaj@gmail.com>
2018-02-11 16:53:52 +0100

    json zagnieżdżona pętla

---
+++ javascript/10-json.js
@@ -0,0 +1,38 @@
+var jsonOsoby = {
+  "osoby": [{
+      imie: "Adam",
+      nazwisko: "Nowacki",
+      wzrost: 180,
+      zainteresowania: [{
+          nazwa: "podroze"
+        },
+        {
+          nazwa: "sport"
+        },
+      ]
+    },
+
+    {
+      imie: "Macin",
+      nazwisko: "Konopka",
+      wzrost: 185,
+      zainteresowania: [{
+          nazwa: "szachy"
+        },
+        {
+          nazwa: "motoryzacja"
+        },
+      ]
+    },
+  ]
+
+}
+
+console.log(jsonOsoby);
+
+jsonOsoby.osoby.forEach( function (element, index) {
+    console.log(element.imie);
+    console.log(element.nazwisko);
+    console.log(element.wzrost);
+    console.log(element.zainteresowania);
+})

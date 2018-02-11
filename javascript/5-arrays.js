7cd2886eff8e538fee4e9ab02bfdc8b76affdeb6
kajetans <66szukaj@gmail.com>
2018-02-11 11:41:51 +0100

    tablice z modyfikacjami

--- javascript/5-arrays.js
+++ javascript/5-arrays.js
@@ -10,5 +10,31 @@ console.log(imiona);
 
 // push dodaje element na końcu tablicy i z automatu przypisze kolejny indeks
 
-imiona.push("Mikołaj");
+imiona.push("Mikołaj", "Adam");
+
+//usuwa ostatni element tablicy
+var usunietyElement = imiona.pop();
+
+//wstawianie elementu na początku tablicy
+imiona.unshift("Kasia", "Michał");
+
+//usuwa element z początku tablicy
+imiona.shift();
+
+//sprawdzenie ileości elementów tablicy
+console.log(imiona.length);
+
 console.log(imiona);
+
+//rozbijanie tablicy na ciąg tekstowy
+
+var tablicaJakoTekst = imiona.join(", ");
+console.log(tablicaJakoTekst);
+
+//odwracanie tablicy
+var imionaReverse = imiona.reverse();
+console.log(imionaReverse);
+
+//sortowanie tablicy
+var posortowana = imiona.sort();
+console.log(posortowana);

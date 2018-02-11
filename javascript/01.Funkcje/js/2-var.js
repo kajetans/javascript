3ee2e41ac62f764658af7dd33d75cd7a48ed36d8
kajetans <66szukaj@gmail.com>
2018-02-11 09:31:32 +0100

    dodanie zmiennej lokalnej

--- javascript/01.Funkcje/js/2-var.js
+++ javascript/01.Funkcje/js/2-var.js
@@ -1,1 +1,10 @@
 // funkcja bez parametrów
+
+var zmiennaAdam = "Adam"; // zmienna globalna widoczna wszędzie
+
+function zmienna lokalna() {
+  var zmiennaAdam = "Zmienna lokalna Adam"; // zmienna lokalna widoczna tylko w funkcji
+  var zmienna2 = "zmienna lokalna 2 widoczna tylko w funcji";
+}
+
+zmiennaLokalna();

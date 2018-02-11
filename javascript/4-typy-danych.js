832bd5d7d954c1abc3654a071f24b880fb033a4f
kajetans <66szukaj@gmail.com>
2018-02-11 10:48:04 +0100

    typy specjalne

--- javascript/4-typy-danych.js
+++ javascript/4-typy-danych.js
@@ -1,7 +1,39 @@
-var liczba = 15.5;
-var text = "Tadzio Wiertełko";
-var prawda = true;
+// var liczba = 15.5;
+// var text = "Tadzio Wiertełko";
+// var prawda = true;
+//
+// // console.log(typeof liczba );
+// // console.log(typeof text );
+// // console.log(typeof prawda );
+//
+//
+// // typ boolean
+// var czyJestSmog = true;
+// if ( czyJestSmog ) {
+//     console.log("Jest Smog");
+// } else {
+//   console.log("Nie ma smogu")
+// }
 
-console.log(typeof liczba );
-console.log(typeof text );
-console.log(typeof prawda );
+//typ number
+// var pensja = 4500;
+// var premia = 5/100;
+//
+// var calkowitaPensja = pensja + ( pensja * premia );
+// console.log(calkowitaPensja);
+
+//typ string
+
+// var pensjaString = "4500";
+// var premiaString = "1200";
+//
+// var calkowitaPensjaString = pensjaString + premiaString;
+// console.log(calkowitaPensjaString);
+
+// typy specjalne
+
+var zmiennaBezWartosci;
+console.log(zmiennaBezWartosci);
+
+var zmiennaNull = null;
+console.log(zmiennaNull);

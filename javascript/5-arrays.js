4ad4da85e4164e63aa7d1688e0d19937e64b04ff
kajetans <66szukaj@gmail.com>
2018-02-11 11:00:37 +0100

    stworzenie tablic

---
+++ javascript/5-arrays.js
@@ -0,0 +1,14 @@
+var  imiona = ["Paweł", "Krzysztof", "Kasia", "Nikola", "Kamil"];
+
+//console.log( imiona[2] ); //Kasia - bo zaczyna liczyć od ZERA!
+console.log(imiona);
+
+imiona[5] = "Monika";
+imiona[6] = "Marcin";
+
+console.log(imiona);
+
+// push dodaje element na końcu tablicy i z automatu przypisze kolejny indeks
+
+imiona.push("Mikołaj");
+console.log(imiona);

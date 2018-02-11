b1a13c0f7b296990560e95a59d2cbf8a0355edb1
kajetans <66szukaj@gmail.com>
2018-02-11 16:37:17 +0100

    pętle; for, for each

--- javascript/9-petle.js
+++ javascript/9-petle.js
@@ -1,24 +1,29 @@
-// var ileRazy = 30;
+// // var ileRazy = 30;
+// //
+// // for( var i = 0; i < ileRazy; i ++ ) {
+// //   var iteracja = i + 1;
+// //   console.log("Iteracja pętli nr " + iteracja);
+// // }
 //
-// for( var i = 0; i < ileRazy; i ++ ) {
-//   var iteracja = i + 1;
-//   console.log("Iteracja pętli nr " + iteracja);
+// var tablica = ["Audi", "Opel", "Honda", "Mazda", "Nissan"];
+//
+// for (var i = 0; i < tablica.length; i++ ) {
+//   console.log( "Tablica! Index = " + i + ", wartość = " + tablica [i] );
 // }
-
-var tablica = ["Audi", "Opel", "Honda", "Mazda", "Nissan"];
-
-for (var i = 0; i < tablica.length; i++ ) {
-  console.log( "Tablica! Index = " + i + ", wartość = " + tablica [i] );
-}
-
-tablica.forEach( function( elementTablicy, indexTablicy ) {
-  console.log("Index: " + indexTablicy + "wartość: " + elementTablicy );
-
-} );
-
-for( var i = 0; i < tablica.length; i++) {
-    if( i == 2 ) {
-      continue;
-    }
-    console.log( i, tablica[i]);
+//
+// tablica.forEach( function( elementTablicy, indexTablicy ) {
+//   console.log("Index: " + indexTablicy + "wartość: " + elementTablicy );
+//
+// } );
+//
+// for( var i = 0; i < tablica.length; i++) {
+//     if( i == 2 ) {
+//       break;
+//     }
+//     console.log( i, tablica[i]);
+// }
+ var iter = 0;
+ while (iter <20 ); {
+ console.log(iter);
+ ++iter;
 }

88d49cfe8d76eb024d336c0392a89c81cddaba12
kajetans <66szukaj@gmail.com>
2018-02-11 12:59:22 +0100

    dodanie obiektu ogłoszenie

--- javascript/6-objects.js
+++ javascript/6-objects.js
@@ -15,18 +15,44 @@
 //
 // osoba.wyswietlSzczgoly();
 
-var osoba = {
-  imie: "",
-  nazwisko: "Nowacki",
-  wzrost: 182,
+// var osoba = {
+//   imie: "",
+//   nazwisko: "Nowacki",
+//   wzrost: 182,
+//
+//     wyswietlSzczgoly: function( paramImie ) {
+//       this.imie = paramImie;
+//         console.log( "osoba to: " + this.imie + " " + this.nazwisko + " " + this.wzrost );
+//     }
+// }
+//
+// console.log(osoba.imie);
+// console.log(osoba["nazwisko"]);
+//
+// osoba.wyswietlSzczgoly("Boguslaw");
+//
+//
+// //dodanie wlasciwosci
+// osoba.wiek = 36;
+// osoba.wyswietlWiek = function() {
+//   console.log(this.wiek);
+// }
+//
+// osoba.wyswietlWiek();
 
-    wyswietlSzczgoly: function( paramImie ) {
-      this.imie = paramImie;
-        console.log( "osoba to: " + this.imie + " " + this.nazwisko + " " + this.wzrost );
+
+class Ogloszenie {
+    constructor (tytul, opis, kontakt, cena) {
+        this.tytul = tytul;
+        this.opis = opis;
+        this.kontakt = kontakt;
+        this.cena = cena;
     }
+
+    wyswietlOgloszenie() {                          //metoda - ma nawiasy po nazwie
+        console.log( "Ogłoszenie! Tytuł: " + this.tytul + "opis; " + this.opis + "kontakt: " + this.kontakt + "cena" + this.cena);
+      }
 }
 
-console.log(osoba.imie);
-console.log(osoba["nazwisko"]);
-
-osoba.wyswietlSzczgoly("Boguslaw");
+var ogloszenie1 = new Ogloszenie( "Sprzedam Opla", "Świeżynka prosto od 80-letniego Niemca", "605-557-421", 16900);
+ogloszenie1.wyswietlOgloszenie();
